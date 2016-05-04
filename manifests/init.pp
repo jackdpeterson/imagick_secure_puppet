4e193999711cc5fd5ae028c669095e45d186ea6f
jackdpeterson <jack.peterson@gmail.com>
2016-05-03 21:31:55 -0700

    add initial files

---
+++ manifests/init.pp
@@ -0,0 +1,15 @@
+class imagick_secure {
+  file { '/etc/ImageMagick/policy.xml':
+    ensure  => present,
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    content => template('imagick_secure/etc/ImageMagick/policy.xml.erb'),
+    require => Exec["imagick_exists"]
+  }
+
+  exec { "imagick_exists":
+    command => '/bin/true',
+    onlyif  => 'test -f /etc/ImageMagick/policy.xml'
+  }
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+## just include the class and if imagick exists ... it'll blow out the policy.xml with a slightly modified ubuntu 14.04 base version w/ extra policies in place
+
+include imagick_secure
